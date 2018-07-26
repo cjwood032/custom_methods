43923cedf9644389e314067384042bca3169695b
cjwood032 <cjwood032@gmail.com>
2018-07-26 17:42:48 -0400

    update methods controller

---
+++ app/controllers/methods_controller.rb
@@ -0,0 +1,91 @@
+class TweetsController < ApplicationController
+  get '/methods' do
+    if logged_in?
+      @methods = Methods.all
+      erb :'methods/methods'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/methods/new' do
+    if logged_in?
+      erb :'methods/new_method'
+    else
+      redirect to '/login'
+    end
+  end
+
+  post '/methods' do
+    if logged_in?
+      if params[:content] == ""
+        redirect to "/methods/new"
+      else
+        @method = current_user.methods.build(content: params[:content])
+        if @tmethod.save
+          redirect to "/methods/#{@method.id}"
+        else
+          redirect to "/methods/new"
+        end
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/methods/:id' do
+    if logged_in?
+      @method = Method.find_by_id(params[:id])
+      erb :'methods/show_method'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/methods/:id/edit' do
+    if logged_in?
+      @method = Method.find_by_id(params[:id])
+      if @method && @method.user == current_user
+        erb :'methods/edit_method'
+      else
+        redirect to '/methods'
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
+  patch '/methods/:id' do
+    if logged_in?
+      if params[:content] == ""
+        redirect to "/methods/#{params[:id]}/edit"
+      else
+        @method = Method.find_by_id(params[:id])
+        if @method && @method.user == current_user
+          if @method.update(content: params[:content])
+            redirect to "/methods/#{@method.id}"
+          else
+            redirect to "/methods/#{@method.id}/edit"
+          end
+        else
+          redirect to '/methods'
+        end
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
+  delete '/methods/:id/delete' do
+    if logged_in?
+      @method = Method.find_by_id(params[:id])
+      if @method && @method.user == current_user
+        @method.delete
+      end
+      redirect to '/methods'
+    else
+      redirect to '/login'
+    end
+  end
+
+end
